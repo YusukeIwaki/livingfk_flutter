180501e594bc530b00c7b593902dcb3e432f5794
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2018-10-21 22:57:56 +0900

    メインコンテンツの一部実装

--- lib/main.dart
+++ lib/main.dart
@@ -61,6 +61,79 @@ class HomeScreen extends StatelessWidget {
     )
     );
   }
+  Widget _topicsListModule(String imageUrl, String category, String subCategory, String title) {
+    return Container(
+      padding: EdgeInsets.all(4.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(imageUrl, width: double.infinity, height: 96.0, fit: BoxFit.fitWidth),
+          Container(
+            margin: EdgeInsets.only(top: 8.0),
+            padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                    decoration: BoxDecoration(border: Border.all(color: Color(0x2A000000))),
+                    padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 6.0),
+                    child: Text(category, style: TextStyle(color: Color(0xFFB60149)), textScaleFactor: 0.66,)
+                ),
+                Container(
+                  margin:EdgeInsets.only(left: 8.0),
+                  child: Text(subCategory, style: TextStyle(color: Color(0x77000000)), textScaleFactor: 0.66,),
+                ),
+              ]
+            )
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(title, maxLines: 2, style: TextStyle(height: 1.5), textScaleFactor: 0.8,)
+          )
+        ],
+      )
+    );
+  }
+  Widget _col1(String imageUrl, String category, String subCategory, String title) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(imageUrl, width: 80.0, height: 80.0, fit: BoxFit.cover,),
+          Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                              decoration: BoxDecoration(border: Border.all(color: Color(0x2A000000))),
+                              padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 6.0),
+                              child: Text(category, style: TextStyle(color: Color(0xFFB60149)), textScaleFactor: 0.66,)
+                          ),
+                          Container(
+                            margin:EdgeInsets.only(left: 8.0),
+                            child: Text(subCategory, style: TextStyle(color: Color(0x77000000)), textScaleFactor: 0.66,),
+                          ),
+                        ]
+                    )
+                ),
+                Container(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                    child: Text(title, maxLines: 2, style: TextStyle(height: 1.5), textScaleFactor: 0.8,)
+                )
+              ],
+            )
+          )
+        ],
+      )
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -134,6 +207,34 @@ class HomeScreen extends StatelessWidget {
           _leftUi("プレゼント", "https://home.livingfk.com/uploads/pictures/21223/content_0649b8ec9f.jpg", "麺 食 喜多方ラーメン ３食セットを5人に ほか"),
           _leftUi("イベントカレンダー", "https://home.livingfk.com/uploads/pictures/21225/content_09919441e8.jpg", "今週末はどこに出かけよう♪おでかけ前にイベントカレンダーをチェック！"),
           _leftUi("リビングツアー&カルチャー", "https://home.livingfk.com/uploads/pictures/21224/content_bdc2a6c138.jpg", "[日帰り]大人気散策スポットと大邸宅へ　麻生大浦荘と九大の森"),
+          Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 8.0),
+              child: Text("ピックアップ", style: TextStyle(fontWeight: FontWeight.bold))
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            child: Divider(height: 4.0, color: Color(0xFFC7AC74)),
+          ),
+          Container(
+            margin: EdgeInsets.all(4.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: _topicsListModule("https://home.livingfk.com/uploads/sprite/item/image/2491/large_abdced5ee3.jpg", "グルメ", "特集（グルメ）", "今年も福岡のパンやスイーツを中心としたお店の特別メニューがズラリ！「ピンクリボングルメ」")
+                ),
+                Expanded(
+                  child: _topicsListModule("https://home.livingfk.com/uploads/sprite/item/image/2711/large_98ea15b98b.jpg", "くらし", "レシピ", "さつまいも消費レシピです！ 「塩バターおいもチップス」")
+                )
+              ]
+            )
+          ),
+          Divider(),
+          _col1("https://home.livingfk.com/uploads/sprite/item/image/2718/thumb_c7203999d1.jpg", "おでかけ", "特集（おでかけ）", "福岡のハロウィンイベント2018＆ハロウィングッズを紹介！"),
+          Divider(),
+          _col1("https://home.livingfk.com/uploads/sprite/item/image/2703/thumb_a2d7c7486c.jpg", "くらし", "チャチャっと簡単♪ステキにおうちDIY", "使わなくなった「木製ハンガー」を簡単リメイク！ 「クローゼットにアクセサリー収納」"),
+          Divider(),
+          _col1("https://home.livingfk.com/uploads/sprite/item/image/2704/thumb_c2505d4980.jpg", "くらし", "ココロとカラダのミネラル", "口唇炎・口唇ヘルペス"),
         ],
       ),
     );
